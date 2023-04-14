4d202af3ccc03c5e1e5e8e4bf17ac9a60076f301
KrainFox <krainfox7.75@gmail.com>
2023-04-14 20:16:23 +0400

    Initial commit

---
+++ in_memory_cache.go
@@ -0,0 +1,19 @@
+package main
+
+type cache map[string]interface{}
+
+func New()cache{
+	return cache{}
+}
+
+func (c cache) Set(key string,value interface{}){
+	c[key] = value
+}
+
+func (c cache) Get(key string)interface{}{
+	return c[key]
+}
+
+func (c cache) Delete(key string){
+	delete(c,key)
+}
